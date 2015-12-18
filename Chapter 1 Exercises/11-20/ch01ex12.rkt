1c8d73ef63bba60d949a749808bbdb658848b494
zero_loss <elmq0022@umn.edu>
2015-12-18 05:50:57 -0600

    Fixed indentation on ch01ex12.rkt

--- Chapter 1 Exercises/11-20/ch01ex12.rkt
+++ Chapter 1 Exercises/11-20/ch01ex12.rkt
@@ -11,7 +11,8 @@
 (define (pascal row col)
   (cond((= col 1) 1)
        ((= col row) 1)
-       (else (+ (pascal (- row 1) (- col 1))(pascal (- row 1) col)))))
+       (else (+ (pascal (- row 1) (- col 1))
+		(pascal (- row 1) col)))))
 
 (pascal 1 1)
 (pascal 2 1)
